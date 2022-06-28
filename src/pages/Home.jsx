e37695fd998e8321b93e67bd4bd56f1248a6cde5
Christy Wright <christywright@Christys-MacBook-Air.local>
2022-06-27 19:15:57 -0500

    learned about reducers. there is an error rn

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import UserResults from '../components/users/UserResults';
+
+function Home() {
+  return (
+    <>
+      {/* Search Component */}
+      <UserResults />
+    </>
+  );
+}
+
+export default Home;
